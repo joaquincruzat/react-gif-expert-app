82e2536a9086229ce3a174de8063b7c84a2f14bd
joaquincruzat <joaquincruzat@gmail.com>
2022-09-26 19:31:35 -0300

    primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["Pokémon"]);
+
+  const onAddCategory = (newCategory) => {
+    // NO USAR PUSH!!! categories.push("Pokémon");
+    if (
+      categories.some(
+        (category) => category.toLowerCase() === newCategory.toLowerCase()
+      )
+    ) {
+      return;
+    }
+
+    setCategories([newCategory, ...categories]);
+  };
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+      <AddCategory onNewCategory={onAddCategory} />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
